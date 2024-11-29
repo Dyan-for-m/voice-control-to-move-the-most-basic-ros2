0374a31573759f1f272a614ed4eb5522c62ff548
Dyan-for-me <3561753156@qq.com>
2024-11-29 20:42:38 +0800

    the frequency of speech is more reasonable

--- whistper_demo_node.py
+++ whistper_demo_node.py
@@ -18,31 +18,40 @@ class WhisperDemoNode(Node):
     def listen(self) -> None:
         try:
             while True:
-                goal = STT.Goal()
+                    goal = STT.Goal()
 
-                self._action_client.wait_for_server()
-                send_goal_future = self._action_client.send_goal_async(goal)
+                    self._action_client.wait_for_server()
+                    send_goal_future = self._action_client.send_goal_async(goal)
 
-                rclpy.spin_until_future_complete(self, send_goal_future)
-                get_result_future = send_goal_future.result().get_result_async()
-                self.get_logger().info("SPEAK")
+                    rclpy.spin_until_future_complete(self, send_goal_future)
+                    get_result_future = send_goal_future.result().get_result_async()
+                    self.get_logger().info("SPEAK")
 
-                rclpy.spin_until_future_complete(self, get_result_future)
-                result: STT.Result = get_result_future.result().result
-                self.get_logger().info(f"I hear: {result.transcription.text}")
-                self.get_logger().info(f"Audio time: {result.transcription.audio_time}")
-                self.get_logger().info(
-                    f"Transcription time: {result.transcription.transcription_time}"
-                )
+                    rclpy.spin_until_future_complete(self, get_result_future)
+                    result: STT.Result = get_result_future.result().result
+                    self.get_logger().info(f"I hear: {result.transcription.text}")
+                    self.get_logger().info(f"Audio time: {result.transcription.audio_time}")
+                    self.get_logger().info(
+                        f"Transcription time: {result.transcription.transcription_time}"
+                    )
 
-                # 发布听到的字符串
-                msg = String()
-                msg.data = result.transcription.text
-                self._publisher.publish(msg)
+                    # 发布听到的字符串
+                    msg = String()
+                    msg.data = result.transcription.text
+                    self._publisher.publish(msg)
 
-                time.sleep(6)
+                    # 根据听到的指令调整循环时间
+                    if result.transcription.text.lower() in [
+                        "forward", "- forward", "go ahead", "go straight", "go forward", "straight", "straight.", "straight", 
+                        "backward", "go back", "go backward", "turn left", "left", "left.", "left", 
+                        "turn right", "right", "right.", "right"
+                    ]:
+                        time.sleep(6)
+                    else:
+                        time.sleep(1)
         except KeyboardInterrupt:
-            self.get_logger().info("Exiting...")
+                self.get_logger().info("Exiting...")
+
 
 def main():
 
